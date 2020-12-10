f8568db808834b6483c8accd1519644190dc72b5
ssbhatt4321 <58531376+ssbhatt4321@users.noreply.github.com>
2020-12-10 12:02:27 -0500

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,23 @@
+import sys
+
+d = sorted([*map(int, sys.stdin)])
+
+#Part 1
+order = [0, *d, max(d) + 3]
+diff = [order[i+1] - order[i] for i in range(len(order)-1)]
+print(diff.count(1) * diff.count(3))
+
+#Part 2
+vals = {}
+def dfs(end):
+	if end in vals:
+		return vals[end]
+	if end <= 0:
+		return end == 0
+	if end not in d:
+		return 0
+	
+	vals[end] = dfs(end - 1) + dfs(end - 2) + dfs(end - 3)
+	return vals[end]
+
+print(dfs(d[-1]))
